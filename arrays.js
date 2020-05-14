f1fdbf28324c9b87646282e50e5ae46cd663038c
Jastien Sandhu <superjastin@gmail.com>
2020-05-14 01:31:02 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,31 @@
+var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array , element){
+  new = [element, ...array]
+  return new;
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  n = array.unshift(element);
+  return n;
+}
+
+function addElementToEndOfArray(array, element){
+  new = [...array, element]
+  return new;  
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  n = array.push(element)
+  return n;
+}
+
+function accessElementInArray(array, index){
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array;
+}
