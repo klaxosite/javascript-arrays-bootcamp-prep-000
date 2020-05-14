a8694824e60d2fdbe7c08c9d20761eb98f9f7400
Jastien Sandhu <superjastin@gmail.com>
2020-05-14 01:38:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -41,5 +41,6 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length - 1)
+  k = array.slice(0, array.length - 1)
+  return k
 }
