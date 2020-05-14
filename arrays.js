0983d881265d28ab0569dc162e46ce36e4f0060a
Jastien Sandhu <superjastin@gmail.com>
2020-05-14 01:43:41 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array , element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  n = array.unShift(element)
+  n = array.unshift(element)
   return n
 }
 
